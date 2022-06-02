b430ff828170f99e4267ec4e00b6d0c8be23fcd6
rehan <rraza9123@gmail.com>
2022-06-02 14:14:27 +0530

    added the images and sliding window

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  slideIndex = 1;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.showSlides(this.slideIndex);
   }
 
+  plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number) {
+    let i;
+    let slides: any = document.getElementsByClassName("mySlides");
+    let dots: any = document.getElementsByClassName("dot");
+    if (n > slides.length) { this.slideIndex = 1 }
+    if (n < 1) { this.slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+  }
 }
