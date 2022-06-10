795c18ef259bb6ded15801b3d7a59ad421bb5a6d
ali-Sohail <57495657+ali-Sohail@users.noreply.github.com>
2022-06-10 11:08:07 +0530

    refactor image folder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  imgPath = "/assets/Images/";
+  imgPath = "/assets/images/";
 
 
   constructor() { }
