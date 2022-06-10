882b59abd2aac1ba765f3101fa8dd79e272bbe1b
ali-Sohail <57495657+ali-Sohail@users.noreply.github.com>
2022-06-10 11:20:16 +0530

    changed project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'new-project'`, () => {
+  it(`should have as title 'qaida-spa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('new-project');
+    expect(app.title).toEqual('qaida-spa');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('new-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('qaida-spa app is running!');
   });
 });
